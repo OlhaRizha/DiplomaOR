a1a465f5af90338d71eef2f03bc1c85db81bfde8
OlhaRizha <olharizha@gmail.com>
2023-02-16 19:47:06 +0200

    Creating UI test for GitHub SignUp page

---
+++ modules/ui/page_objects/sign_up_page.py
@@ -0,0 +1,31 @@
+from modules.ui.page_objects.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+#Створення дочірнього класу SignUpPage(BasePage), на основі якого будуть створені тести 
+# для перевірки веб сторінки Sign Up на сайті GitHub
+class SignUpPage(BasePage):
+    URL='https://github.com/signup'
+
+    def __init__(self):
+        super().__init__()
+
+#Створення методу об'єкта що відправляє запит GET на URL адресу
+    def go_to(self):
+        self.driver.get(SignUpPage.URL)
+
+#Створення методу об'єкта, що виконує ввід певних даних для реєстрації на GitHub,
+#  використовуючи електронну пошту вже зареєстрованого користувача
+    def try_email(self,email):
+        regist_elem=self.driver.find_element(By.ID, "email")
+        regist_elem.send_keys(email)
+        
+#Cтворення методу об'єкта, що перевіряє наявність помилки під час введення 
+# електронної пошти вже існуючого користувача для виконання реєстрації на GitHub
+    def check_error(self,expected_error):
+        error_elem=self.driver.find_element(By.ID, "email-err")
+        return error_elem==expected_error
+    
+#Cтворення методу об'єкта, що перевіряє відповідність заголовку певної веб сторінки на GitHub
+#  та повертає результат цієї перевірки
+    def check_title(self,expected_title):
+        return self.driver.title==expected_title
---
+++ tests/ui/test_ui_signUp_page_object.py
@@ -0,0 +1,27 @@
+from modules.ui.page_objects.sign_up_page import SignUpPage
+import pytest
+import time
+
+#Створення теста з міткою signup, що створює об'єкт класу SignUpPage,
+# перевіряє можливість відкрити веб сторінку Sign Up сайту GitHub, 
+# перевіряє можливість ввести електронну пошту вже існуючого користувача для здійснення реєстрації та
+# перевіряє якою буде помилка у відповідь на цю дію,
+# перевіряє чи назва даної сторінки така як очікується, закриває цю сторінку
+@pytest.mark.signup
+def test_check_incorrect_data_for_sign_up_page():
+    sign_up_page=SignUpPage()
+
+    sign_up_page.go_to()
+
+    time.sleep(2)
+
+    sign_up_page.try_email("olharizha@gmail.com")
+
+    time.sleep(2)
+
+    sign_up_page.check_error("Email is invalid or already taken")
+
+    assert sign_up_page.check_title("Join GitHub · GitHub")
+
+
+    sign_up_page.close()
